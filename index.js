85251c183840a235ff7603af05d55a766b125d5c
Tanuka Das <das.tanuka@yahoo.com>
2019-03-23 01:19:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function whisper(string) {
 }
 function logShout(string){
   //console.log("string.toUpperCase()");
-  return string.toUpperCase()
+  //return string.toUpperCase()
   console.log(string);
 }
