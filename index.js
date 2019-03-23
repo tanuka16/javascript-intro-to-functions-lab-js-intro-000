242f58c27f6670d172c7e857b65e2b047ab3bd69
Tanuka Das <das.tanuka@yahoo.com>
2019-03-23 01:22:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ function shout(string) {
 }
 function whisper(string) {
   return string.toLowerCase()
+  
 }
 function logShout(string){
   console.log(string.toUpperCase());
+
 }
 function logWhisper(string){
   console.log(string.toLowerCase());
