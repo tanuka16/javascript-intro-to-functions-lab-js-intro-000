02556f6bb10890f11040cc6ef51fc2f86341b2df
Tanuka Das <das.tanuka@yahoo.com>
2019-03-23 01:44:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string.toLowerCase() === lowercase){
+  if (string.toLowerCase()){
     return "I can't hear you!"
   }
-  else if (string.toUpperCase() === uppercase){
+  else if (string.toUpperCase()){
     return "YES INDEED!"
   }
   else if (string === "I love you, Grandma."){
